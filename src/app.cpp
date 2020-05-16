7e2502b53694786f573f2efa40600f2e9d362590
liushuai <770722922@qq.com>
2020-05-17 03:24:05 +0800

    connect test

--- src/app.cpp
+++ src/app.cpp
@@ -36,4 +36,66 @@ void init_app() {
     init_uart();
 
     xTaskCreate(&task_blink, (const signed char*)"startup", 2048, NULL, 1, NULL);
+    xTaskCreate([](void*) {
+        wifi_set_event_handler_cb(
+                [](System_Event_t *evt) {
+                    LOGD("wifi_set_event_handler_cb: %d", evt->event_id);
+                    return ;
+                    switch(evt->event_id){
+                        case EVENT_STAMODE_CONNECTED:
+                            LOGD("Have got IP!");
+                            break;
+                        case EVENT_STAMODE_DISCONNECTED:
+                            LOGD("Have got IP!");
+                            break;
+                        defaut:
+                            LOGD("Have'n got IP, id=%d", evt->event_id);
+                            break;
+                    }
+                });
+
+        if (not wifi_set_opmode(STATION_MODE)) {
+            LOGE("wifi_set_opmode");
+        }
+
+        for(;;) {
+            static scan_config config;
+            wifi_station_scan(&config, [](void *arg, STATUS status) {
+                LOGD("wifi_station_scan: %d", config.channel);
+            });
+
+            {
+                // connect
+                station_config stationConfig{};
+                strcpy((char*)stationConfig.ssid, "test_1");
+                strcpy((char*)stationConfig.password, "12345678");
+                wifi_station_set_config(&stationConfig);
+            }
+
+            auto status = wifi_station_get_connect_status();
+            LOGD("status: %d", status);
+            auto ret = wifi_station_connect();
+            LOGD("wifi_station_connect: %d", ret);
+            /**
+                static scan_config config;
+                wifi_station_scan(&config, [](void *arg, STATUS status) {
+                    LOGD("scan: %d", config.channel);
+                });
+             */
+            status = wifi_station_get_connect_status();
+            LOGD("status: %d", status);
+
+            if (STATION_CONNECTING == status) {
+                vTaskDelay(10000 / portTICK_RATE_MS);
+                for(;;) {
+                    if (STATION_GOT_IP == status) {
+                        ip_info info{};
+                        auto r = wifi_get_ip_info(STATION_IF, &info);
+                        LOGD("wifi_get_ip_info: %d, %d", r, info.ip.addr);
+                    }
+                }
+            }
+        }
+    }, (const signed char*)"startup", 2048, NULL, 1, NULL);
 }
+
